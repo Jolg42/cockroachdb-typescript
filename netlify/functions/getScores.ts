5b35ddfbacf189f03c4d3758483cd22345d64d92
Joël Galeran <Jolg42@users.noreply.github.com>
2022-05-09 21:15:58 +0000

    test

--- netlify/functions/getScores.ts
+++ netlify/functions/getScores.ts
@@ -1,37 +1,48 @@
 import { Handler } from '@netlify/functions'
 import { PrismaClient } from '@prisma/client'
+import { getOpenSSLVersion, resolveDistro } from '@prisma/get-platform/dist/getPlatform'
 
-const prisma = new PrismaClient();
+
+// const prisma = new PrismaClient();
 
 const handler: Handler = async (event, context) => {
   // load all scores from the database and include player name
   // from the players table.
-  const allScores = await prisma.player_scores.findMany({
-    include: {
-      players: {
-        select: { 
-          name: true
-        }
-      }
-    },
-    orderBy: {
-      score: 'desc'
-    }
-  });
+  // const allScores = await prisma.player_scores.findMany({
+  //   include: {
+  //     players: {
+  //       select: { 
+  //         name: true
+  //       }
+  //     }
+  //   },
+  //   orderBy: {
+  //     score: 'desc'
+  //   }
+  // });
+  const libssl = await getOpenSSLVersion()
+  const distro = await resolveDistro()
+  console.log(process.env.NETLIFY)
+  console.log(libssl)
+  console.log(distro)
 
   return {
     statusCode: 200,
-    body: JSON.stringify(allScores.map(score => (
-      // flatten player name into score entry 
-      { id: score.id, name: score.players.name, score: score.score }
-    ))
-    , (key, value) =>
-      // need to add a custom serializer because CockroachDB IDs map to
-      // JavaScript BigInts, which JSON.stringify has trouble serializing.
-      typeof value === 'bigint'
-        ? value.toString()
-        : value 
-    )
+    body: JSON.stringify({
+      libssl,
+      distro,
+    })
+    // body: JSON.stringify(allScores.map(score => (
+    //   // flatten player name into score entry 
+    //   { id: score.id, name: score.players.name, score: score.score }
+    // ))
+    // , (key, value) =>
+    //   // need to add a custom serializer because CockroachDB IDs map to
+    //   // JavaScript BigInts, which JSON.stringify has trouble serializing.
+    //   typeof value === 'bigint'
+    //     ? value.toString()
+    //     : value 
+    // )
   }
 }
 
